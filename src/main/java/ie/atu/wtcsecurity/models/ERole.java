06a5b368ec4faf365dc5b96e1e77d36db4d7f20d
Denis Costello <G00396309@atu.ie>
2024-03-30 15:03:02 +0000

    Added Role and User models and Repositories

---
+++ src/main/java/ie/atu/wtcsecurity/models/ERole.java
@@ -0,0 +1,7 @@
+package ie.atu.wtcsecurity.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/ie/atu/wtcsecurity/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package ie.atu.wtcsecurity.repository;
+
+import ie.atu.wtcsecurity.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
