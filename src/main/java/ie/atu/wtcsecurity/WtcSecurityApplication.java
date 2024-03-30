798e7ab53c7c4e69e525660fc547940cc5b3271a
Denis Costello <G00396309@atu.ie>
2024-03-30 14:54:24 +0000

    First commit

---
+++ src/main/java/ie/atu/wtcsecurity/WtcSecurityApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtcSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/wtcsecurity/WtcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
